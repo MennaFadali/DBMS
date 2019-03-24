d7f69ee6b56fec15fcc0b741bc7e9ade70ac0869
MennaFadali <menafadali98@gmail.com>
2019-03-24 22:05:36 +0200

    Adding Bit Operations to the BitMap

--- src/moonchild/BitMap.java
+++ src/moonchild/BitMap.java
@@ -15,6 +15,61 @@ public class BitMap {
             bits.add('0');
     }
 
+    static BitMap and(BitMap one, BitMap two) {
+        BitMap ans = new BitMap();
+        for (int i = 0; i < one.bits.size(); i++)
+            ans.bits.add(BitOperations(one.bits.get(i), two.bits.get(i), 0));
+        return ans;
+    }
+
+    static BitMap or(BitMap one, BitMap two) {
+        BitMap ans = new BitMap();
+        for (int i = 0; i < one.bits.size(); i++)
+            ans.bits.add(BitOperations(one.bits.get(i), two.bits.get(i), 1));
+        return ans;
+    }
+
+    static BitMap xor(BitMap one, BitMap two) {
+        BitMap ans = new BitMap();
+        for (int i = 0; i < one.bits.size(); i++)
+            ans.bits.add(BitOperations(one.bits.get(i), two.bits.get(i), 2));
+        return ans;
+    }
+
+    static BitMap not(BitMap one) {
+        BitMap ans = new BitMap();
+        for (int i = 0; i < one.bits.size(); i++)
+            ans.bits.add(not(one.bits.get(i)));
+        return ans;
+    }
+
+    static char not(char a) {
+        return a == '1' ? '0' : '1';
+    }
+
+    static char BitOperations(char a, char b, int type) {
+        //add => 0
+        //or => 1
+        //xor => 2
+        switch (type) {
+            case 0:
+                if (a == '0' || b == '0') return '0';
+                else return '1';
+            case 1:
+                if (a == '1' || b == '1') return '1';
+                else return '0';
+            case 2:
+                if (a == b) return '0';
+                else
+                    return '1';
+        }
+        return '0';
+    }
+
+    int get(int idx) {
+        return (bits.get(idx) == '0') ? 0 : 1;
+    }
+
     void set(int idx) {
         bits.set(idx, '1');
     }
@@ -24,8 +79,7 @@ public class BitMap {
     }
 
     void toggle(int idx) {
-        char neval = (bits.get(idx) == '0' ? '1' : '0');
-        bits.set(idx, neval);
+        bits.set(idx, not(bits.get(idx)));
     }
 
     void addBitAfter(int idx, int val) {
